c158cb3883688e758d45dd38ab6d90c144a086da
sweetAme <reskeleton@gmail.com>
2021-03-03 15:41:03 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
+module.exports = function towelSort(matrix) {
+    if (matrix) {
+      let output = [];
 
-// You should implement your task here.
+      for (let row = 0; row < matrix.length; row++) {
+        if (row % 2 == 0) {
+          // inline case 
+          for (let item = 0; item < matrix[row].length; item++) {
+            output.push(matrix[row][item]);
+          }
+        } else {
+          // reversed case
+          for (let item = matrix[row].length - 1; item >= 0; item--) {
+            output.push(matrix[row][item]);
+          }
+        }
+      }
+      
+      return output;
 
-module.exports = function towelSort (matrix) {
-  return [];
+    } else {
+        return [];
+    }
 }
